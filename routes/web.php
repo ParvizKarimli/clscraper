bfd127fa3726c99a21bd9b62c660500138bf26ee
Parviz Karimli <devparviz@gmail.com>
2019-07-01 11:28:09 +0400

    Removed dashboard

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,3 @@ Route::get('/', function () {
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
-
-Route::get('/dashboard', 'DashboardController@index');
